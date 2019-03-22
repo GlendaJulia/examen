67eeaedc8b2b54542f689ed0a48894a20f021ea4
Glenda Julia <glenda.merma@tecsup.edu.pe>
2019-03-22 12:41:12 -0500

    Sin estilo

--- lab03/miapp/routes/index.js
+++ lab03/miapp/routes/index.js
@@ -13,8 +13,23 @@ router.get('/', function (req, res, next) {
     })
 });
 
-router.post('/', function (req, res, next) {
-    fs.readFile('./index.html', function (err, html) {
+router.get('/solicitud', function (req, res, next) {
+    fs.readFile('solicitud.html', function (err, html) {
+        var html_string = html.toString();
+        res.writeHead(200, { 'Content-type': 'text/html' });
+        res.write(html_string);
+        res.end();
+    })
+});
+
+router.post('/solicitud', function (req, res, next) {
+    res.render('respuesta', {
+        title: 'Solicitud enviada, muchas gracias por visitarnos :D'
+    });
+});
+
+router.get('/galeria', function (req, res, next) {
+    fs.readFile('galeria.html', function (err, html) {
         var html_string = html.toString();
         res.writeHead(200, { 'Content-type': 'text/html' });
         res.write(html_string);
